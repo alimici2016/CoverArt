72b29532f84df98bb7fcdda0260149b713a3785f
Hamsa Alimci <alimici2016@gmail.com>
2021-11-15 14:39:55 -0600

    Using my Index as file hold on to my reducers and Sagas for now, completed both for fetching all movies.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { takeEvery, put } from 'redux-saga/effects';
 
 import store from './redux/store';
 
 import App from './components/App/App';
+
+function* rootSaga() {
+  yield takeEvery('FETCH_MOVIES', fetchAllMovies);
+}
+
+function* fetchAllMovies() {
+  try {
+    const movies = yield axios.get('/api/movie');
+    console.log('get all:', movies.data);
+    yield put({ type: 'SET_MOVIES', payload: movies.data });
+
+  } catch {
+    console.log('get all error');
+  }
+};
+
+const movies = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_MOVIES':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  combineReducers({
+    movies,
+  }),
+  // Add sagaMiddleware to our store
+  applyMiddleware(sagaMiddleware, logger),
+);
+
 
 ReactDOM.render(
   <Provider store={store}>
